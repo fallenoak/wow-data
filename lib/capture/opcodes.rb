e0939c9b2578375aff47c5e799fe896c029345ee
fallenoak <github2@uh-oh.co>
2015-07-27 11:11:38 -0500

    Switched packet classes in opcodes directory to symbols.
    The class names are immutable and Ruby happily accepts symbols for const_get, so
    there's no good reason to keep them as strings.

--- lib/capture/opcodes.rb
+++ lib/capture/opcodes.rb
@@ -1,20 +1,20 @@
 module WOW::Capture
   module Opcodes
-    INVALID_PACKET_CLASS_NAME    = 'Invalid'
-    UNHANDLED_PACKET_CLASS_NAME  = 'Unhandled'
+    INVALID_PACKET_CLASS_NAME    = :Invalid
+    UNHANDLED_PACKET_CLASS_NAME  = :Unhandled
 
     # Messages from server
     module SMSG
       DIRECTORY = {
-        0x1102 => ['SMSG_AUTH_CHALLENGE',   'AuthChallenge'],
-        0x0C28 => ['SMSG_ON_MONSTER_MOVE',  'OnMonsterMove']
+        0x1102 => ['SMSG_AUTH_CHALLENGE',   :AuthChallenge],
+        0x0C28 => ['SMSG_ON_MONSTER_MOVE',  :OnMonsterMove]
       }
     end
 
     # Messages from client
     module CMSG
       DIRECTORY = {
-        0x045A => ['CMSG_AUTH_SESSION',     'AuthSession']
+        0x045A => ['CMSG_AUTH_SESSION',     :AuthSession]
       }
     end
   end
