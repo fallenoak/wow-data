9403b189ad0a9e0dfe58aef6761533847ed313a5
fallenoak <github2@uh-oh.co>
2015-07-27 09:37:38 -0500

    Started to flesh out OnMonsterMove packet class.

--- lib/capture/packets/smsg/on_monster_move.rb
+++ lib/capture/packets/smsg/on_monster_move.rb
@@ -1,6 +1,13 @@
 module WOW::Capture::Packets::SMSG
   class OnMonsterMove < WOW::Capture::Packets::Base
+    attr_reader :guid, :position_x, :position_y, :position_z
+
     def parse!
+      @guid = read_guid128
+
+      @position_x = read_float
+      @position_y = read_float
+      @position_z = read_float
     end
   end
 end
