6fc06d8839a1972f4949c56d654a4f448c7bb956
fallenoak <github2@uh-oh.co>
2015-08-14 08:37:38 -0500

    Cleaned up parsing for SMSG_QUERY_CREATURE_RESPONSE.

--- lib/capture/packets/smsg/query_creature_response.rb
+++ lib/capture/packets/smsg/query_creature_response.rb
@@ -1,9 +1,9 @@
 module WOW::Capture::Packets::SMSG
   class QueryCreatureResponse < WOW::Capture::Packets::Base
-    attr_reader :entry_id, :name, :female_name, :flags1, :flags2, :creature_type_id,
-      :creature_family_id, :creature_rank, :kill_credits, :creature_display_ids, :hp_multiplier,
-      :energy_multiplier, :creature_movement_info_id, :expansion, :flag_quest, :title,
-      :alternate_title, :cursor_name, :quest_item_ids
+    attr_reader :entry_id, :name, :female_name, :flags_1, :flags_2, :creature_type_id,
+      :creature_family_id, :creature_rank, :creature_kill_credit_ids, :creature_display_ids,
+      :health_multiplier, :power_multiplier, :creature_movement_info_id, :required_expansion,
+      :quest_flags, :title, :alternate_title, :cursor_name, :quest_item_ids
 
     def parse!
       raw_entry_id = read_uint32
@@ -36,17 +36,19 @@ module WOW::Capture::Packets::SMSG
         @female_name = read_string if alternate_name_lengths[i] > 1
       end
 
-      @flags1 = read_uint32
-      @flags2 = read_uint32
+      @flags_1 = read_uint32
+      @flags_2 = read_uint32
 
       @creature_type_id = read_int32
       @creature_family_id = read_int32
-      @creature_rank = read_int32
 
-      @kill_credits = []
+      creature_rank_index = read_int32
+      @creature_rank = parser.defs.creature_ranks[creature_rank_index]
+
+      @creature_kill_credit_ids = []
 
       2.times do
-        @kill_credits << read_uint32
+        @creature_kill_credit_ids << read_uint32
       end
 
       @creature_display_ids = []
@@ -55,13 +57,17 @@ module WOW::Capture::Packets::SMSG
         @creature_display_ids << read_uint32
       end
 
-      @hp_muliplier = read_float
-      @energy_multiplier = read_float
+      @health_multiplier = read_float
+      @power_multiplier = read_float
 
       quest_items_count = read_int32
+
       @creature_movement_info_id = read_uint32
-      @expansion = read_uint32
-      @flag_quest = read_int32
+
+      required_expansion_index = read_int32
+      @required_expansion = parser.defs.expansions[required_expansion_index]
+
+      @quest_flags = read_int32
 
       @title = nil
       @alternate_title = nil
