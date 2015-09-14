5ebd1b6c83dd5d28dcd7e5c5314c1519c05d43f7
fallenoak <github2@uh-oh.co>
2015-09-13 23:32:19 -0500

    Made definitions loading dynamic.
    Rather than explicitly enumerating all definitions files, definitions
    that live in the appropriate directory are now required automatically.

--- lib/common.rb
+++ lib/common.rb
@@ -8,23 +8,6 @@ require_relative 'common/bit_array'
 
 require_relative 'common/definitions'
 
-require_relative 'common/definitions/19027/dbc'
-
-require_relative 'common/definitions/20182/db2'
-
-require_relative 'common/definitions/20253/opcodes'
-require_relative 'common/definitions/20253/classes'
-require_relative 'common/definitions/20253/creature_ranks'
-require_relative 'common/definitions/20253/emotes'
-require_relative 'common/definitions/20253/expansions'
-require_relative 'common/definitions/20253/guid_types'
-require_relative 'common/definitions/20253/map_types'
-require_relative 'common/definitions/20253/inventory_types'
-require_relative 'common/definitions/20253/item_classes'
-require_relative 'common/definitions/20253/item_qualities'
-require_relative 'common/definitions/20253/item_subclasses'
-require_relative 'common/definitions/20253/power_types'
-require_relative 'common/definitions/20253/races'
-require_relative 'common/definitions/20253/text_emotes'
-require_relative 'common/definitions/20253/unit_flags'
-require_relative 'common/definitions/20253/update_fields'
+Dir.glob("#{File.dirname(__FILE__)}/common/definitions/**/*.rb").each do |definitions_file|
+  require definitions_file
+end
