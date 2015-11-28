4bb9ceb1650eb8d1f2c722a14f20fb732e45d3f7
fallenoak <fallenoak@uh-oh.co>
2015-11-28 16:34:25 -0600

    Added client builds module.
    The module is capable of returning the client build number in use at a given
    timestamp.

--- lib/common.rb
+++ lib/common.rb
@@ -5,7 +5,7 @@ require 'digest/sha1'
 require 'stringio'
 
 require_relative 'common/bit_array'
-
+require_relative 'common/client_builds'
 require_relative 'common/definitions'
 
 Dir.glob("#{File.dirname(__FILE__)}/common/definitions/**/*.rb").each do |definitions_file|
