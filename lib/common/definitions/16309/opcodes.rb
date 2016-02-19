772ac5e3307ac0b88ce613c6119d19291a5d8bb2
fallenoak <fallenoak@uh-oh.co>
2016-02-19 16:15:53 -0600

    Started moving away from tc_value in favor of generic label.

--- lib/common/definitions/16309/opcodes.rb
+++ lib/common/definitions/16309/opcodes.rb
@@ -3,18 +3,18 @@ module WOW::Definitions
     namespace :opcodes do
       table :cmsg do
         # Implemented
-        #e   0x05B1,   :PlayerLogin,                             tc_value: 'CMSG_PLAYER_LOGIN'
+        #e   0x05B1,   :PlayerLogin,                             label: 'CMSG_PLAYER_LOGIN'
       end
 
       table :smsg do
         # Implemented
-        e   0x0009,   :Chat,                                    tc_value: 'SMSG_CHAT'
-        #e   0x2D15,   :AttackStart,                             tc_value: 'SMSG_ATTACK_START'
-        #e   0x0934,   :AttackStop,                              tc_value: 'SMSG_ATTACK_STOP'
-        #e   0x4542,   :AuthChallenge,                           tc_value: 'SMSG_AUTH_CHALLENGE'
-        #e   0x6E17,   :OnMonsterMove,                           tc_value: 'SMSG_ON_MONSTER_MOVE'
-        #e   0x6024,   :QueryCreatureResponse,                   tc_value: 'SMSG_QUERY_CREATURE_RESPONSE'
-        #e   0x6E04,   :QueryPlayerNameResponse,                 tc_value: 'SMSG_QUERY_PLAYER_NAME_RESPONSE'
+        e   0x0009,   :Chat,                                    label: 'SMSG_CHAT'
+        #e   0x2D15,   :AttackStart,                             label: 'SMSG_ATTACK_START'
+        #e   0x0934,   :AttackStop,                              label: 'SMSG_ATTACK_STOP'
+        #e   0x4542,   :AuthChallenge,                           label: 'SMSG_AUTH_CHALLENGE'
+        #e   0x6E17,   :OnMonsterMove,                           label: 'SMSG_ON_MONSTER_MOVE'
+        #e   0x6024,   :QueryCreatureResponse,                   label: 'SMSG_QUERY_CREATURE_RESPONSE'
+        #e   0x6E04,   :QueryPlayerNameResponse,                 label: 'SMSG_QUERY_PLAYER_NAME_RESPONSE'
       end
     end
   end
