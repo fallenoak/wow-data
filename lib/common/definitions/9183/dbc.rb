4a53a6b788a4213633fbd4898792b58ab719f458
fallenoak <github@fallenoak.me>
2016-07-13 23:41:02 -0500

    Added ItemDisplayInfo structure def for build 9183

--- lib/common/definitions/9183/dbc.rb
+++ lib/common/definitions/9183/dbc.rb
@@ -6,6 +6,21 @@ module WOW::Definitions
         e   1,    :file_name,                   type: :string,  tc_value: ''
         e   2,    :file_path,                   type: :string,  tc_value: ''
       end
+
+      table :item_display_info do
+        e   0,    :id,                          type: :uint32
+        e   1,    :left_model,                  type: :string
+        e   2,    :right_model,                 type: :string
+        e   3,    :left_model_texture,          type: :string
+        e   4,    :right_model_texture,         type: :string
+        e   5,    :icon_1_name,                 type: :string
+        e   6,    :icon_2_name,                 type: :string
+        e   7,    :geoset_1_group,              type: :uint32
+        e   8,    :geoset_2_group,              type: :uint32
+        e   9,    :geoset_3_group,              type: :uint32
+        e   10,   :flags,                       type: :uint32
+        e   11,   :spell_visual_id,             type: :uint32
+      end
     end
   end
 end
